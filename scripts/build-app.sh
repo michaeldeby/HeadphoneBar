7a2a949339ccfea6edda8b36c1ed2b6143e78a4e
Michael de By <michaeldeby@users.noreply.github.com>
2026-09-13 23:26:58 +0200

    Add a simple blue headphone app icon

--- scripts/build-app.sh
+++ scripts/build-app.sh
@@ -26,6 +26,7 @@ else
 fi
 cp -R ThirdPartyNotices "$APP/Contents/Resources/"
 cp LICENSE "$APP/Contents/Resources/LICENSE"
+cp Assets/AppIcon.icns "$APP/Contents/Resources/AppIcon.icns"
 cat > "$APP/Contents/Info.plist" <<'PLIST'
 <?xml version="1.0" encoding="UTF-8"?>
 <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
@@ -34,6 +35,7 @@ cat > "$APP/Contents/Info.plist" <<'PLIST'
 <key>CFBundleDisplayName</key><string>HeadphoneBar</string>
 <key>CFBundleIdentifier</key><string>local.headphonebar.app</string>
 <key>CFBundleExecutable</key><string>HeadphoneBar</string>
+<key>CFBundleIconFile</key><string>AppIcon</string>
 <key>CFBundlePackageType</key><string>APPL</string>
 <key>CFBundleShortVersionString</key><string>0.1.0</string>
 <key>CFBundleVersion</key><string>1</string>
